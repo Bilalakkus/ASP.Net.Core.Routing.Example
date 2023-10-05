ec1fd824de81a08f4f0f2be03cae4254c90e9060
Bilal Akkuş <Bilalakkus65@hotmail.com>
2023-10-05 15:04:24 +0300

    Add project files.

---
+++ ASP.Net.Core.Routing.Example/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net.Core.Routing.Example.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AdminController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ ASP.Net.Core.Routing.Example/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.Net.Core.Routing.Example.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Article()
+        {
+            return View();
+        }
+    }
+}
